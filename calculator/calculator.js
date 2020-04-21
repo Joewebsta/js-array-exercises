1060f220de2045ed782f747e8c0258da7a4d4a8a
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-21 15:16:47 -0400

    complete calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,43 @@
-function add () {
-	
+function add (a,b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	let sum = 0;
+	array.forEach(element => {
+		sum += element;
+	});
+
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let total = 1;
+	array.forEach(element => {
+		total *= element;
+	});
+
+	return total;
 }
 
-function power() {
-	
+function power(base, exponent) {
+	return base ** exponent;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let total = 1;
+
+	for (let i = num; i >= 1; i--){
+		total *= i;
+	}
+
+	return total;
 }
+
 
 module.exports = {
 	add,
