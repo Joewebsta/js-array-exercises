175f034723af997dbe2625fcbe5ff382334ae6b2
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-23 16:25:51 -0400

    Complete palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
-
-}
-
+const palindromes = function(str) {
+    let strArray = str.toLowerCase().split('');
+    
+    let cleanStrArray = strArray.filter(item => {
+        let asciiCode = item.charCodeAt(0);
+  
+      return  asciiCode >= 97 && asciiCode <= 122
+    })
+  
+    return cleanStrArray.join('') === cleanStrArray.reverse().join('');
+  }
+  
 module.exports = palindromes
