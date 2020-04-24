a48849d8cc2991247af1150f0de3ab8e795323c1
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-24 10:06:57 -0400

    improve text formatting

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -2,43 +2,45 @@ const caesar = function(str, shift) {
     let splitArr = str.split('');
 
     let shiftArr = splitArr.map((letter) => {
-    let asciiCode = letter.charCodeAt(0);
+        let asciiCode = letter.charCodeAt(0);
 
-    if (shift > 25) {
-      shift = shift % 26;
-    }
+        if (shift > 25) {
+            shift = shift % 26;
+        }
 
-    //Determine if uppercase
-    if (asciiCode >= 65 && asciiCode <= 90) {
-      let shiftCode = asciiCode + shift;
+        //Determine if uppercase
+        if ( asciiCode >= 65 && asciiCode <= 90 ) {
+            let shiftCode = asciiCode + shift;
 
-      //Determine if shift causes ascii code to exceed upcase range
-      if (shiftCode > 90) {
-        shiftCode -= 26;
-      } else if (shiftCode < 65) {
-        shiftCode += 26;
-      }
+            //Determine if shift causes ascii code to exceed upcase range
+            if ( shiftCode > 90 ) {
+                shiftCode -= 26;
+            } else if ( shiftCode < 65 ) {
+                shiftCode += 26;
+            }
 
-      //Convert shifted ascii code back letter
-      let shiftLetter = String.fromCharCode(shiftCode);
-      return shiftLetter;
-    } else if(asciiCode >= 97 && asciiCode <= 122) {
-      let shiftCode = asciiCode + shift;
+            //Convert shifted ascii code back letter
+            let shiftLetter = String.fromCharCode(shiftCode);
+            return shiftLetter;
 
-      //Determine if shift causes ascii code to exceed lowcase range
-      if (shiftCode > 122) {
-        shiftCode -= 26;
-      } else if (shiftCode < 97) {
-        shiftCode += 26;
-      }
+        } else if ( asciiCode >= 97 && asciiCode <= 122 ) {
+            let shiftCode = asciiCode + shift;
 
-      //Convert shifted ascii code back letter
-      let shiftLetter = String.fromCharCode(shiftCode);
-      return shiftLetter;
-    } else {
-      return String.fromCharCode(asciiCode)
-    }
-  })
+            //Determine if shift causes ascii code to exceed lowcase range
+            if (shiftCode > 122) {
+                shiftCode -= 26;
+            } else if (shiftCode < 97) {
+                shiftCode += 26;
+            }
+
+            //Convert shifted ascii code back letter
+            let shiftLetter = String.fromCharCode(shiftCode);
+            return shiftLetter;
+
+        } else {
+            return String.fromCharCode(asciiCode)
+        }
+    });
 
   return shiftArr.join('');
 }
