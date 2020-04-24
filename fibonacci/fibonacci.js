5c44e975202072ebfb89a51a6c5af4f9381a77e5
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-24 17:39:34 -0400

    Complete fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
-}
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0) { return 'OOPS'};
+  
+    let prev = 0;
+    let current = 1;
+    let sum = 1;
+  
+    for (let i = 1; i <= num; i++) {
+      if ( num === 1 ) {
+        return 1;
+      } else if (i > 1) {
+        sum = current + prev;
+        prev = current;
+        current = sum;
+      }
+    }
+    return sum;
+  }  
 
 module.exports = fibonacci
