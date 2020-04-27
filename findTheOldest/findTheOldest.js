eeec17ab1dca454b59ae2898ce048393fbc3cd15
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-27 08:53:13 -0400

    Complete findTheOldest JS exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    people.map(person => {
+        if (!person.yearOfDeath) { 
+          return person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    
+    let sortedPeople = people.sort((a, b) => {
 
+    let personA = a.yearOfDeath - a.yearOfBirth;
+    let personB = b.yearOfDeath - b.yearOfBirth;
+
+    return personA < personB ? 1 : -1;
+    });
+
+    return sortedPeople[0];
 }
 
 module.exports = findTheOldest
